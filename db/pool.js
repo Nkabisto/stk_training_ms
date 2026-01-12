6dd218b3d7d645865882b135e3658d0bdf08c084
Lionel <nkabisto@gmail.com>
2026-01-12 15:13:46 +0200

    The beginning

---
+++ db/pool.js
@@ -0,0 +1,18 @@
+import process from 'node:process';
+import pkg from  "pg";
+import 'dotenv/config';
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  //connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false}:false,
+  max:30, // Limit concurrent connections (default is 30);
+  idleTimeoutMillis: 300000, //Close idle clients after 5 min
+  connectionTimeoutMillis: 120000 // Give up after 10s trying to connect
+});
---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE applicants(
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	sa_id CHAR(13) NOT NULL UNIQUE,
+	email TEXT NOT NULL,
+	phone VARCHAR(10),
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE appointment_slots(
+	id SERIAL PRIMARY KEY,
+	start_time TIMESTAMP NOT NULL,
+	end_time TIMESTAMP NOT NULL,
+	max_capacity INTEGER DEFAULT 10,
+	current_bookings INTEGER DEFAULT 0 CHECK (current_bookings <= max_capacity)
+);
+
+CREATE TABLE booking_slots(
+	applicant_id CHAR(13) REFERENCES applicants(sa_id),
+	slot_id INTEGER REFERENCES appointment_slots(id),
+	created_at TIMESTAMP DEFAULT NOW(),
+	PRIMARY KEY (applicant_id, slot_id) -- Added composite key
+);
